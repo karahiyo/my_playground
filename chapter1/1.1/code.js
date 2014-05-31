9251ddd8b25cf962d4a74715d6c77926ed101012
karahiyo <a.ryuklnm@gmail.com>
2014-05-31 19:24:27 +0900

    自分用workspace追加

---
+++ chapter1/1.1/code.js
@@ -0,0 +1,24 @@
+//元のコード
+Function originailCode() {
+    var res = "";
+    var w = 80;
+    var h = 40;
+    var synchronized = 30;
+
+    for (var y = 0; y < h; y++) {
+        for (var x = 0; x < w; x++) {
+            var dstnx = Match.sqrt(
+                Math.pow(w / 2 - x, 2)
+                + Math.pow((h / 2 - y) * 2, 2)
+            );
+            if (dstnc < sz) {
+                res += "*";
+            } else {
+                res += "-";
+            }
+        }
+        res += "\n";
+    } 
+
+    return res;
+}
