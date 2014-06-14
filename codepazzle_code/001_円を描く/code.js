11e107f05c6816ce0bef139e8e4db1e041efc555
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-06-14 18:28:20 +0900

    001 done

--- codepazzle_code/001_円を描く/code.js
+++ codepazzle_code/001_円を描く/code.js
@@ -1,28 +1,7 @@
-//= 元のコード
 function originalCode() {
-	// 変数の初期化
-	var res = "";
-	var w = 80;
-	var h = 40;
-	var sz = 30;
-
-	// 処理
-	for (var y = 0; y < h; y ++) {
-		for (var x = 0; x < w; x ++) {
-			var dstnc = Math.sqrt(
-				  Math.pow(w / 2 - x, 2)
-				+ Math.pow((h / 2 - y) * 2, 2)
-			);
-			if (dstnc < sz) {
-				res += "*";
-			} else {
-				res += "-";
-			}
-		}
-		res += "\n"
-	}
-
-	// 結果を戻して終了
-	return res;
+    r = ""
+	for (y=a=x=40;y+a;)
+        r += "-*\n"[x+a?x*x--+y*y<900|0:(x=a,y-=2,2)]
+	return r
 }
 
