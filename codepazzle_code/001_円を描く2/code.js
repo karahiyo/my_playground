26922731fff6012cbf142b4bc093675baa85b35d
karahiyo <a.ryuklnm@gmail.com>
2014-07-23 23:35:28 +0900

    い” く” そ” ーす” と”

--- codepazzle_code/001_円を描く2/code.js
+++ codepazzle_code/001_円を描く2/code.js
@@ -1,14 +1,9 @@
 function originalCode() {
-	var r = "", sA  = 1024, sB  = 256;
+	r = "", s  = 256;
 
-	for (var y = -24; y < 24; y ++, r += "\n") {
-		for (var x = -48; x < 48; x ++) {
-			var d= x*x + 4*y*y;
-            var q=0;
-			if (d< sA) q++;
-			if (d< sB) q++;
-            r += q;
-		}
+	for (y = x= h=48; y + h; x+h||(y-=2, x=h, r += "\n")) {
+			d= x--*x + y*y;
+            r += (d<s*4)+(d<s);
 	}
 
 	return r;
